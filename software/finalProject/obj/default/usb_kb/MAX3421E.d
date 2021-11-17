c67bbeff288b1a79fcf81d7fc6c9fad0ad7191f5
GiuseppeIII <josephbabbo1@gmail.com>
2021-11-17 14:44:57 -0600

    software clean

---
+++ software/finalProject/obj/default/usb_kb/MAX3421E.d
@@ -0,0 +1,49 @@
+obj/default/usb_kb/MAX3421E.o: usb_kb/MAX3421E.c \
+ ../finalProject_bsp/system.h ../finalProject_bsp/linker.h \
+ usb_kb/../usb_kb/project_config.h \
+ usb_kb/../usb_kb/../usb_kb/GenericMacros.h \
+ usb_kb/../usb_kb/../usb_kb/GenericTypeDefs.h \
+ usb_kb/../usb_kb/../usb_kb/HID.h usb_kb/../usb_kb/../usb_kb/MAX3421E.h \
+ usb_kb/../usb_kb/../usb_kb/transfer.h \
+ usb_kb/../usb_kb/../usb_kb/usb_ch9.h usb_kb/../usb_kb/../usb_kb/USB.h \
+ ../finalProject_bsp//drivers/inc/altera_avalon_spi.h \
+ ../finalProject_bsp//HAL/inc/alt_types.h \
+ ../finalProject_bsp//drivers/inc/altera_avalon_spi_regs.h \
+ ../finalProject_bsp//HAL/inc/io.h \
+ ../finalProject_bsp//HAL/inc/alt_types.h \
+ ../finalProject_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../finalProject_bsp//HAL/inc/sys/alt_stdio.h
+
+../finalProject_bsp/system.h:
+
+../finalProject_bsp/linker.h:
+
+usb_kb/../usb_kb/project_config.h:
+
+usb_kb/../usb_kb/../usb_kb/GenericMacros.h:
+
+usb_kb/../usb_kb/../usb_kb/GenericTypeDefs.h:
+
+usb_kb/../usb_kb/../usb_kb/HID.h:
+
+usb_kb/../usb_kb/../usb_kb/MAX3421E.h:
+
+usb_kb/../usb_kb/../usb_kb/transfer.h:
+
+usb_kb/../usb_kb/../usb_kb/usb_ch9.h:
+
+usb_kb/../usb_kb/../usb_kb/USB.h:
+
+../finalProject_bsp//drivers/inc/altera_avalon_spi.h:
+
+../finalProject_bsp//HAL/inc/alt_types.h:
+
+../finalProject_bsp//drivers/inc/altera_avalon_spi_regs.h:
+
+../finalProject_bsp//HAL/inc/io.h:
+
+../finalProject_bsp//HAL/inc/alt_types.h:
+
+../finalProject_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../finalProject_bsp//HAL/inc/sys/alt_stdio.h:

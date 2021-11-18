ed7809d0fadba8da7c26c6316c188c65bc3d5e61
GiuseppeIII <josephbabbo1@gmail.com>
2021-11-18 16:34:29 -0600

    better wall counters2

--- quartus_files/ball.sv
+++ quartus_files/ball.sv
@@ -22,7 +22,7 @@ module  ball ( input Reset, frame_clk,
 					output		resetB);
     
     logic [9:0] Ball_X_Pos, Ball_X_Motion, Ball_Y_Pos, Ball_Y_Motion, Ball_Size;
-	 logic [2:0] ledgeCountVal, redgeCountVal, tedgeCountVal, bedgeCountVal;
+	 logic [3:0] ledgeCountVal, redgeCountVal, tedgeCountVal, bedgeCountVal;
 	 logic 		 resetBall;
 	 logic [3:0] score_left, score_right;
 	 
@@ -119,38 +119,38 @@ module  ball ( input Reset, frame_clk,
 					end
 					
 					
-				if (bedgeCountVal > 0 && bedgeCountVal < 12)
+				if (bedgeCountVal > 0 && bedgeCountVal < 15)
 					begin
 					  bedgeCountVal <= bedgeCountVal + 1;
 					end
-				else if (bedgeCountVal == 12)
+				else if (bedgeCountVal == 15)
 					begin
 						bedgeCountVal <= 0;
 					end
 					
-				if (tedgeCountVal > 0 && tedgeCountVal < 12)
+				if (tedgeCountVal > 0 && tedgeCountVal < 15)
 					begin
 					  tedgeCountVal <= tedgeCountVal + 1;
 					end
-				else if (tedgeCountVal == 12)
+				else if (tedgeCountVal == 15)
 					begin
 						tedgeCountVal <= 0;
 					end
 					
-				if (ledgeCountVal > 0 && ledgeCountVal < 12)
+				if (ledgeCountVal > 0 && ledgeCountVal < 15)
 					begin
 					  ledgeCountVal <= ledgeCountVal + 1;
 					end
-				else if (ledgeCountVal == 12)
+				else if (ledgeCountVal == 15)
 					begin
 						ledgeCountVal <= 0;
 					end
 					
-				if (redgeCountVal > 0 && redgeCountVal < 12)
+				if (redgeCountVal > 0 && redgeCountVal < 15)
 					begin
 					  redgeCountVal <= redgeCountVal + 1;
 					end
-				else if (redgeCountVal == 12)
+				else if (redgeCountVal == 15)
 					begin
 						redgeCountVal <= 0;
 					end
